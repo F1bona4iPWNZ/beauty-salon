d203a35b8d4aa22db8cfc7c7cc221ba64471dc76
F1bona4iPWNZ <60856881+F1bona4iPWNZ@users.noreply.github.com>
2022-02-09 00:04:31 +0200

    full done

--- js/main.js
+++ js/main.js
@@ -8,9 +8,49 @@ $('.our__team-container').slick({
     arrows:false,
     //autoplay:true,
     //autoplaySpeed:3000,
+
+});
+$('.servises__wrapper').slick({
+  dots: true,
+  dotsClass: 'slick-dots',
+  infinite: true,
+  adaptiveHeight: true,
+  slidesToShow:4,
+  slidesToScroll:1,
+  arrows:false,
+  responsive: [
+    {
+        breakpoint: 1070, // tablet breakpoint
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 790, // mobile breakpoint
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    },
+    {
+      breakpoint: 628, // mobile breakpoint
+      settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+      }
+  }
+]
+  //autoplay:true,
+  //autoplaySpeed:3000,
+
 });
 $( document ).ready(function() {
     $( ".resetButton" ).on('click',function(){ 
       $('.rating > input').prop('checked','')
   });
     });
+    $('.header_btn-menu').on('click', function(){
+      $('.menu__ul-mobile').slideToggle();
+  });
+  
